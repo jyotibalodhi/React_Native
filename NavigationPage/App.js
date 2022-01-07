e6c37584a50e7e466b417ce92ae0c7bb55b38d6b
Jyoti <jyotibalodhi@gmail.com>
2022-01-07 11:01:31 +0530

    Updated

--- NavigationPage/App.js
+++ NavigationPage/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {View, Text} from 'react-native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-ionicons';
 import screenOne from './src/screens/screenOne';
 import screenTwo from './src/screens/screenTwo';
 import screenThree from './src/screens/screenThree';
 import screenFour from './src/screens/screenFour';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={screenOne} />
-        <Stack.Screen name="Screen Two" component={screenTwo} />
-        <Stack.Screen name="Screen Three" component={screenThree} />
-        <Stack.Screen name="Screen Four" component={screenFour} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Screen Two') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            } else if (route.name === 'Screen Three') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            } else if (route.name === 'Screen Four') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={screenOne} />
+        <Tab.Screen name="Screen Two" component={screenTwo} />
+        <Tab.Screen name="Screen Three" component={screenThree} />
+        <Tab.Screen name="Screen Four" component={screenFour} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- NavigationPage/src/screens/screenOne.js
+++ NavigationPage/src/screens/screenOne.js
@@ -1,31 +1,24 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text} from 'react-native';
 
 const screenOne = ({navigation}) => {
   return (
     <View
       style={{
         flex: 1,
-        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         backgroundColor: 'blue',
         padding: 20,
       }}>
-      <Button
-        color={'green'}
-        title="Go to page 2"
-        onPress={() => navigation.navigate('Screen Two')}></Button>
-
-      <Button
-        color={'green'}
-        title="Go to page 3"
-        onPress={() => navigation.navigate('Screen Three')}></Button>
-
-      <Button
-        color={'green'}
-        title="Go to page 4"
-        onPress={() => navigation.navigate('Screen Four')}></Button>
+      <Text
+        style={{
+          color: 'white',
+          fontWeight: 'bold',
+          fontSize: 25,
+        }}>
+        Home Screen!
+      </Text>
     </View>
   );
 };
